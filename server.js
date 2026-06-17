21b356b2a01141e0ea2bbab17d21a0182834738e
jyminimarket1-design <jyminimarket1@gmail.com>
2026-06-17 14:53:48 -0400

    Actualizar

--- server.js
+++ server.js
@@ -57,7 +57,14 @@ const whitelist = [
 ];
 
 app.use(cors({
-  origin: true,
+  origin: (origin, callback) => {
+    // Allow requests from whitelisted origins or any Vercel domain
+    if (!origin || whitelist.includes(origin) || origin.includes('vercel.app')) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
