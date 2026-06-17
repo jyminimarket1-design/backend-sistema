429d2ed436890d616f0e84198645e8aad1883c37
jyminimarket1-design <jyminimarket1@gmail.com>
2026-06-17 15:41:25 -0400

    feat: actualizacion del server

--- server.js
+++ server.js
@@ -69,11 +69,7 @@ app.use(cors({
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
-}));
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
-}));
+}))
 
 // 1.5 SLA TIMEOUT (Fail Fast: corta cualquier request que exceda 1.5s)
 app.use(slaTimeout);
